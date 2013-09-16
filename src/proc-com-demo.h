6610339f5339da52c525ed1244905b54c4d42b27
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-16 19:02:55 -0400

    nap max

--- src/proc-com-demo.h
+++ src/proc-com-demo.h
@@ -16,12 +16,17 @@
  */
 #define SEND_SIG(pid, sig) kill(pid, sig)
 
+//max number of seconds for child to sleep -- see dbg build
+#ifndef NAP_MAX
+#define NAP_MAX 5 
+#endif
+
 /**
  * Just ridiculous
  */
-#define TAKE_A_NAP sleep((rand() % 5) + 1)
+#define TAKE_A_NAP sleep((rand() % NAP_MAX) + 1)
 #define LUCKY rand() & 1
-#define ONE_TO_FIVE_SECS (rand() % 5) + 1
+
 
 /* **************************************************************
  * Globals
@@ -45,7 +50,7 @@ void handle_signals(int);
 
 /**
  * Child Signal Loop
- * Waits 1-5 seconds then sends either a SIGUSR1 or SIGUSR2.
+ * Waits 1-NAP_MAX seconds then sends either a SIGUSR1 or SIGUSR2.
  */
 void child_loop(void) {
   while(true) {
